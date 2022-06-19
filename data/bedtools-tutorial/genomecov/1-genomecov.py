9619589dd84d1b78d0ac6309726d2eea76c5b538
Hao Hou <haohou302@gmail.com>
2022-06-19 16:56:42 -0700

    Add example for genomecov

---
+++ data/bedtools-tutorial/genomecov/1-genomecov.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from pygrass import parse_args, IntervalFile, load_genome_file
+import genomecovlib
+
+parse_args()
+
+load_genome_file("../data/genome.txt")
+
+file = IntervalFile("../data/exons.bed")
+
+genomecovlib.genomecov(file)
